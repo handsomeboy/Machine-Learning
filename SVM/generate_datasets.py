19fd37c7df8efeed7450b267bb3805d190df97db
Martin Rosenfeld <marrosenfeld@gmail.com>
2016-04-04 15:35:07 -0500

    first version

--- SVM/generate_datasets.py
+++ SVM/generate_datasets.py
@@ -4,13 +4,14 @@ import numpy as np
 from matplotlib.colors import ListedColormap
 import plot_utils as pu
 from sklearn import preprocessing
+import svm as svm
 
 def mapFeatures(x, degree):
     poly = preprocessing.PolynomialFeatures(degree)
     return poly.fit_transform(x)
 
 def func(x):
-    return 1 * (np.dot(x, [-1,-2,-1])>0)
+    return 1 * (np.dot(x, [2,-1])>0)
 
 def func2(x):
     val = np.dot(x, [-1,-2,-1])
@@ -20,13 +21,45 @@ def func2(x):
     return 1 * (val>0)
 
 def main():
-    X = np.empty([100,2])
-    X[:,0] = np.matrix((random.sample(range(-1000, 1000), 100))) / 1000
-    X[:,1] = np.matrix((random.sample(range(-1000, 1000), 100))) / 1000
-    X=mapFeatures(X,1)
+    X = np.empty([500,2])
+    X[:,0] = np.matrix((random.sample(range(-10000, 10000), 500))) / float(1000)
+    X[:,1] = np.matrix((random.sample(range(-10000, 10000), 500))) / float(1000)
 
     #linearly separable
-    # y = func(X)
+    y = func(X)
+
+    #plot data and decision surface
+    ax = plt.gca()
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    ax.scatter(X[:,0], X[:,1], c=(y == 1), cmap=cm_bright)
+    plt.xlabel("X1")
+    plt.ylabel("X2")
+    pu.plot_surface(X,y, X[:, 0], X[:,1], disc_func=func, ax=ax)
+    plt.show()
+
+    #train svm
+    w,w0 = svm.train(X,y,c=999999999999999)
+    print(w,w0)
+
+    #plot result
+    predicted_labels = svm.classify_all(X,w,w0)
+    print(svm.getAccuracy(y,predicted_labels))
+    for i in range(X.shape[0]):
+        if(y[i] != predicted_labels[i]):
+            print(X[i,:])
+    ax = plt.gca()
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    ax.scatter(X[:, 0], X[:, 1], c=(y == 1), cmap=cm_bright)
+    plt.xlabel("X1")
+    plt.ylabel("X2")
+    pu.plot_surface2(X[:,0], X[:,1], w,w0)
+    plt.show()
+
+
+    #not separable
+    # y = np.empty([100,1])
+    # for i in range(X.shape[0]):
+    #     y[i] = func2(X[i,:])
     #
     # # plot data and decision surface
     # ax = plt.gca()
@@ -36,20 +69,6 @@ def main():
     # plt.ylabel("X2")
     # pu.plot_surface(X,y, X[:, 1], X[:, 2], disc_func=func, ax=ax)
     # plt.show()
-
-    #not separable
-    y = np.empty([100,1])
-    for i in range(X.shape[0]):
-        y[i] = func2(X[i,:])
-
-    # plot data and decision surface
-    ax = plt.gca()
-    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-    ax.scatter(X[:,1], X[:,2], c=(y == 1), cmap=cm_bright)
-    plt.xlabel("X1")
-    plt.ylabel("X2")
-    pu.plot_surface(X,y, X[:, 1], X[:, 2], disc_func=func, ax=ax)
-    plt.show()
 
 if __name__ == "__main__":
     main()
--- SVM/svm.py
+++ SVM/svm.py
@@ -1,3 +1,66 @@
 import numpy as np
 import cvxopt
-def
+from cvxopt import solvers, matrix
+import operator
+from sklearn import metrics
+
+def train(X,y,c=9999):
+    m = X.shape[0]
+    n = X.shape[1]
+    #p
+    P = np.dot(y,y.T) * np.dot(X,X.T)
+    P = matrix(P, tc='d')
+    #q
+    q = np.empty([m, 1])
+    q.fill(-1)
+    q = matrix(q, tc='d')
+    #G
+    G = np.zeros([2*m, m])
+    np.fill_diagonal(G,-1)
+    np.fill_diagonal(G[m:,:],1)
+    G = matrix(G)
+    #h
+    h = np.empty([2*m,1])
+    h.fill(0)
+    for i in range(m,2*m):
+        h[i]=c
+    h = matrix(h,tc='d')
+    #A
+    A = y.T
+    A = matrix(A, tc='d').T
+    #b
+    b=np.zeros([1,1])
+    b=matrix(b, tc='d')
+
+    #solve for alphas
+    solvers.options['maxiters'] = 1000
+    solution = solvers.qp(P, q, G, h, A, b)
+    alphas = np.matrix(solution['x'])
+
+    #calculat w
+    w = np.zeros([1,n])
+    for i in range(m):
+        w += alphas[i] * y[i] * X[i,:]
+
+    #calculate w0
+    support_vectors_idx = [ k for k in range(len(alphas)) if alphas[k] > 0.1 ]
+    # index, value = max(enumerate(alphas), key=operator.itemgetter(1))
+
+    w0 = 0
+    for i in range(len(support_vectors_idx)):
+        print("support vector: {}".format(X[i,:]))
+        w0 += (y[i] - np.dot(w,X[i,:]))
+    w0 = w0/len(support_vectors_idx)
+    return w,w0
+
+def classify(x,w,w0):
+    return 1 * (np.dot(w, x) + w0 > 0)
+
+def classify_all(X,w,w0):
+    predictedLabels = list()
+    for i in range(0,X.shape[0]):
+        predictedLabels.append(classify(X[i,:],w,w0))
+    return predictedLabels
+
+def getAccuracy(labels,predictedLabels):
+    return metrics.accuracy_score(labels, predictedLabels)
